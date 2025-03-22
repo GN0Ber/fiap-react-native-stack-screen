f29184a508f728565fb8edd67268c46eaf995122
FelipeHeilmann <felipeheilmannm@gmail.com>
2025-03-21 22:31:12 -0300

    feat: details page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 // Importa o componente status bar
-import { StatusBar } from "expo-status-bar"
+import { StatusBar } from "expo-status-bar";
 //Importa os modelos necessários para navegação no aplicativo.
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native";
 
 // Importa os modelos necessários para navegação no aplicativo
-import { NavigationContainer } from "@react-navigation/native"
-import { createStackNavigator } from "@react-navigation/stack"
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import HomeScreen from "./screens/HomeScreen"
+import HomeScreen from "./screens/HomeScreen";
+import DetailsScreen from "./screens/DetailsScreen";
 
 type RootStackParamList = {
-  Home: undefined
-  Details: undefined
-}
+  Home: undefined;
+  Details: undefined;
+};
 
-const Stack = createStackNavigator<RootStackParamList>()
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
@@ -25,7 +26,12 @@ export default function App() {
           component={HomeScreen}
           options={{ title: "HomePage" }}
         />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{ title: "DetailsPage" }}
+        ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
